b82e45ff45fc6faf6d4ad9e43ef23cae48831457
Liuying Wang <wly@Liuyings-MacBook-Pro.local>
2021-04-25 20:56:25 -0700

    parse base code

---
+++ TestParse/app/src/main/java/com/wly/testparse/MainActivity.java
@@ -0,0 +1,28 @@
+package com.wly.testparse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.ParseObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseObject firstObject = new  ParseObject("FirstClass");
+        firstObject.put("message","Hey ! First message from android. Parse is now connected");
+        firstObject.saveInBackground(e -> {
+            if (e != null){
+                Log.e("MainActivity", e.getLocalizedMessage());
+            }else{
+                Log.d("MainActivity","Object saved.");
+            }
+        });
+
+    }
+}
